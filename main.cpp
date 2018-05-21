eb0d088abe40d6b30fb5c3ca47a14f6f9f169651
Jordan Goulder <jordan.goulder@gmail.com>
2018-05-21 18:48:44 -0400

    Add ability to show multiple images and show original beside output

--- main.cpp
+++ main.cpp
@@ -5,42 +5,54 @@ using namespace cv;
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
-        printf("Usage %s <Image Path>\n", argv[0]);
+    if (argc < 2) {
+        printf("Usage %s < Image(s) >\n", argv[0]);
         return -1;
     }
 
-    Mat image;
-    image = imread(argv[1], IMREAD_COLOR);
+    int index = 1;
 
-    resize(image, image, Size(640, 480));
+    while (index < argc) {
+        Mat image;
+        image = imread(argv[index++], IMREAD_COLOR);
 
-    medianBlur(image, image, 13);
-    
-    if (!image.data) {
-        printf("No image data\n");
-        return -2;
+        if (!image.data) {
+            printf("No image data\n");
+            continue;
+        }
+
+        resize(image, image, Size(640, 480));
+
+        Mat imageBlur;
+        medianBlur(image, imageBlur, 13);
+
+        Mat imageHsv;
+
+        cvtColor(imageBlur, imageHsv, COLOR_BGR2HSV);
+
+        std::vector<Mat> channels(3);
+
+        split(imageHsv, channels);
+
+        Mat satDivVal;
+
+        divide(channels[1], channels[2], satDivVal, 40.0);
+
+        Mat water;
+        threshold(satDivVal, water, 64, 255, THRESH_BINARY_INV);
+
+        Mat displayImage(image.size().height, image.size().width+image.size().width, CV_8UC3);
+
+        cvtColor(water, water, COLOR_GRAY2BGR);
+
+        hconcat(image, water, displayImage);
+
+        imshow("Result", displayImage);
+
+        if ( (char) 27 == (char) waitKey(0)) {
+            return 0;
+        }
     }
-
-    Mat imageHsv;
-
-    cvtColor(image, imageHsv, COLOR_BGR2HSV);
-
-    std::vector<Mat> channels(3);
-
-    split(imageHsv, channels);
-
-    Mat satDivVal;
-
-    divide(channels[1], channels[2], satDivVal, 40.0);
-
-    Mat water;
-    threshold(satDivVal, water, 64, 255, THRESH_BINARY_INV);
-
-    imshow("Original", image);
-    imshow("Water", water);
-
-    waitKey(0);
 
     return 0;
 }
